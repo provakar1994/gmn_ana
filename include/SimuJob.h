0b0bb74569bac5c4dd2cf00afece8b1a1bd44144
Provakar Datta <pdbforce@ifarm1801.jlab.org>
2023-05-30 14:31:35 -0400

    Made necessary modifications to read latest version of both g4sbs and simc summary files

--- include/SimuJob.h
+++ include/SimuJob.h
@@ -10,8 +10,8 @@ typedef struct SimuJob {
   double ntried{0};         // # of simulated events tried to get the requested
   double genvol{0};         // generation volume (sr for g4sbs, MeV*sr2 for SIMC)
   double lumi{0};           // luminosity (ub^-1 for SIMC & Hz/cm2 for g4sbs) 
-  double charge{0};         // C, total charge per simulation job (rel. for simc)
   double ebeam{0};          // GeV, uncorrected beam energy (rel. for g4sbs)
+  double charge{0};         // C, total charge per simulation job (rel. for simc)
   double ibeam{0};          // A, beam current (rel. for g4sbs)
 
   // constructor 
@@ -19,6 +19,19 @@ typedef struct SimuJob {
 
   // sets data by reading MC job summary (exclusively for util::ReadSimuJobSummary function)
   void SetDataSimuJob(std::vector<std::string> const &data) {
+    /* sfname    = data[0]; */
+    /* rfname    = data[1]; */
+    /* generator = data[2]; */
+    /* ngenreq   = stod(data[3]); */
+    /* ntried    = stod(data[4]); */
+    /* genvol    = stod(data[5]); */
+    /* lumi      = stod(data[6]); */
+    /* if (generator.compare("simc")==0) */
+    /*   charge  = stod(data[7])/1000.; */
+    /* if (generator.compare("g4sbs")==0) { */
+    /*   ebeam   = stod(data[7]);       */
+    /*   ibeam   = stod(data[8])*1e-6;       */
+    /* } */
     sfname    = data[0];
     rfname    = data[1];
     generator = data[2];
@@ -26,16 +39,31 @@ typedef struct SimuJob {
     ntried    = stod(data[4]);
     genvol    = stod(data[5]);
     lumi      = stod(data[6]);
+    ebeam     = stod(data[7]);      
     if (generator.compare("simc")==0)
-      charge  = stod(data[7])/1000.;
+      charge  = stod(data[8])/1000.;
     if (generator.compare("g4sbs")==0) {
-      ebeam   = stod(data[7]);      
       ibeam   = stod(data[8])*1e-6;      
     }
   }
 
   // define an ostream operator to print to screen conveniently
   friend std::ostream& operator <<(std::ostream &out, const SimuJob& sjob) {
+    /* out << " ------------" << std::endl; */
+    /* out << " Simu. file name    : " << sjob.sfname << std::endl; */
+    /* out << " Replayed file name : " << sjob.rfname << std::endl; */
+    /* out << " Generator          : " << sjob.generator << std::endl; */
+    /* out << " # events requested : " << sjob.ngenreq << std::endl; */
+    /* out << " # events tried     : " << sjob.ntried << std::endl; */
+    /* out << " Generation vol.    : " << sjob.genvol << std::endl; */
+    /* out << " Luminosity         : " << sjob.lumi << std::endl; */
+    /* if (sjob.generator.compare("simc")==0) */
+    /*   out << " Charge (C)         : " << sjob.charge << std::endl; */
+    /* if (sjob.generator.compare("g4sbs")==0) { */
+    /*   out << " Ebeam (GeV)        : " << sjob.ebeam << std::endl; */
+    /*   out << " Ibeam (A)          : " << sjob.ibeam << std::endl; */
+    /* } */
+    /* out << " ------------" << std::endl << std::endl; */
     out << " ------------" << std::endl;
     out << " Simu. file name    : " << sjob.sfname << std::endl;
     out << " Replayed file name : " << sjob.rfname << std::endl;
@@ -44,12 +72,11 @@ typedef struct SimuJob {
     out << " # events tried     : " << sjob.ntried << std::endl;
     out << " Generation vol.    : " << sjob.genvol << std::endl;
     out << " Luminosity         : " << sjob.lumi << std::endl;
+    out << " Ebeam (GeV)        : " << sjob.ebeam << std::endl;
     if (sjob.generator.compare("simc")==0)
       out << " Charge (C)         : " << sjob.charge << std::endl;
-    if (sjob.generator.compare("g4sbs")==0) {
-      out << " Ebeam (GeV)        : " << sjob.ebeam << std::endl;
+    if (sjob.generator.compare("g4sbs")==0) 
       out << " Ibeam (A)          : " << sjob.ibeam << std::endl;
-    }
     out << " ------------" << std::endl << std::endl;
     return out;
   }
--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -609,20 +609,27 @@ namespace util_pd {
 	  data.push_back(sfname.Data());
 	  data.push_back(rfname.Data());
 	  data.push_back(generator);
-	  if (generator.compare("simc") == 0) {
-	    data.push_back(temp[1]); //ngenreq
-	    data.push_back(temp[2]); //nthrown
-	    data.push_back(temp[5]); //genvol(MeV*sr2)
-	    data.push_back(temp[4]); //lumi(ub^-1)
-	    data.push_back(temp[3]); //charge(mC)
-	  }else	if (generator.compare("g4sbs") == 0) {
-	    data.push_back(temp[1]); //ngenreq
-	    data.push_back(temp[2]); //nthrown
-	    data.push_back(temp[5]); //genvol(sr)
-	    data.push_back(temp[6]); //lumi(Hz/cm2)
-	    data.push_back(temp[3]); //ebeam(GeV) 
-	    data.push_back(temp[4]); //ibeam(A)
-	  }
+	  // if (generator.compare("simc") == 0) {
+	  //   data.push_back(temp[1]); //ngenreq
+	  //   data.push_back(temp[2]); //nthrown
+	  //   data.push_back(temp[5]); //genvol(MeV*sr2)
+	  //   data.push_back(temp[4]); //lumi(ub^-1)
+	  //   data.push_back(temp[3]); //charge(mC)
+	  // }else	if (generator.compare("g4sbs") == 0) {
+	  //   data.push_back(temp[1]); //ngenreq
+	  //   data.push_back(temp[2]); //nthrown
+	  //   data.push_back(temp[5]); //genvol(sr)
+	  //   data.push_back(temp[6]); //lumi(Hz/cm2)
+	  //   data.push_back(temp[3]); //ebeam(GeV) 
+	  //   data.push_back(temp[4]); //ibeam(A)
+	  // }
+	  data.push_back(temp[1]); //ngenreq
+	  data.push_back(temp[2]); //nthrown
+	  data.push_back(temp[3]); //genvol(MeV*sr2)
+	  data.push_back(temp[4]); //lumi(ub^-1)
+	  data.push_back(temp[5]); //ebeam(GeV)
+	  if (generator.compare("simc") == 0) data.push_back(temp[6]);  //charge(mC)
+	  if (generator.compare("g4sbs") == 0) data.push_back(temp[6]); //ibeam(muA)
 
 	  temp_sj.SetDataSimuJob(data);
 	  sjobs.push_back(temp_sj);
