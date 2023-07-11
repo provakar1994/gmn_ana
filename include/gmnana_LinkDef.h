bd2ec79b9bf1e932c78778d2fccc5215c8a6cea2
Provakar Datta <pdbforce@ifarm1901.jlab.org>
2023-07-11 08:41:30 -0400

    Link definition file added

---
+++ include/gmnana_LinkDef.h
@@ -0,0 +1,15 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+// general utility namespace 
+//#pragma link C++ namespace gmnana; 
+
+//#pragma link C++ function setrootvar::setbranch(TTree*, std::string, std::string, void*);
+//#pragma link C++ function setrootvar::setbranch(TTree*, std::string, std::vector<std::string>, std::vector<void*>);
+//#pragma link C++ function setrootvar::setbranch(TTree*, std::string, std::vector<std::string>, std::vector<void*>, int);
+//#pragma link C++ function setrootvar::setbranch(TTree*, std::string, std::vector<std::string>, std::vector<void*>, std::vector<int>);
+
+#endif
